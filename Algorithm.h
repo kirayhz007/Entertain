3e8046d0d327edab363209dfc926a139359d736c
kirayhz007 <363912097@qq.com>
2014-09-19 10:54:35 +0800

    Create Algorithm.h
    some sorting Algorithms written in my last holiday

---
+++ Algorithm.h
@@ -0,0 +1,14 @@
+#ifndef _Algorithm_H
+#define _Algorithm_H
+#include<iostream>
+using namespace std;
+
+void quickSort1(int a[],int n);
+void insertSort1(int a[],int n);
+void selectSort1(int a[],int n);
+void bubbleSort1(int a[],int n);
+void shellSort1(int a[],int n);
+void heapSort1(int a[],int n);
+void mergeSort1(int a[],int n);
+
+#endif
